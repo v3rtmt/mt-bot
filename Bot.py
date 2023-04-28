19e6dd91fb2544340fe786a1e2419ae57ce582c2
unknown <v3rtmt@gmail.com>
2023-04-28 00:36:08 -0500

    0.1.35

--- Bot.py
+++ Bot.py
@@ -13,37 +13,46 @@ Bot = Blueprint('Bot', __name__)
 # Devuelve la pagina para crear un nuevo bot
 @Bot.route('/Bot/create', methods=['GET', 'POST'])
 def createbot():
+	print(request.form)
 	user = mongo.db.Users.find_one({"username": session['username']})
-	
-	if (request.form.get("apiKey") == None) and 'username' in session:
-		return render_template('Dashboard/botcreate.html', user=user, apikey=False, apiSecret=False)
-
-	elif (request.form.get("apiKey") != None):
+ 
+	if request.method == 'POST':
 		
 		if (request.form.get("apiKey") == ''):
 			return render_template('Dashboard/botcreate.html', user=user, apiKey=True, apiSecret=False)
 
-		if (request.form.get("apiSecret") == ''):
+		if (request.form.get("SecretKey") == ''):
 			return render_template('Dashboard/botcreate.html', user=user, apiKey=False, apiSecret=True)
 
+		if (float(request.form.get("orderAmount")) > 100) and (request.form.get("amountType") == "%"): orderAmount = 100
+		elif (float(request.form.get("orderAmount")) < 0) and (request.form.get("amountType") == "%"): orderAmount = 0
+		else: orderAmount = float(request.form.get("orderAmount"))
+		
+		if int(request.form.get("leverage")) > 100: leverage = 100
+		elif int(request.form.get("leverage")) < 1: leverage = 1
+		else: leverage = int(request.form.get("leverage"))
+  
+		isSimulated = True if request.form.get("isSimulated") else False
+  
 		newBot = {
    			"ownerID"         : user['_id'],
 			"ownerUser"       : user['username'],
-			"isEnabled"       : True,
 			"type"            : "Crypto Futures",
+			"isEnabled"       : True,
+			"isSimulated"     : isSimulated,
 			"settings" : {
 				"exchange" : {
-					"name"        : "Binance",
+					"name"        : request.form.get("exchange"),
 					"apiKey"      : request.form.get("apiKey"),
-					"apiSecret"   : request.form.get("apiSecret"),
+					"secretKey"   : request.form.get("secretKey"),
 					"apiKey2"     : request.form.get("apiKey2"),
-					"apiSecret2"  : request.form.get("apiSecret2")
+					"secretKey2"  : request.form.get("secretKey2")
 				},
-				"pair"        : "ETH/BUSD",
+				"pair"        : request.form.get("pair"),
 				"tradeType"   : "isolated",
-				"leverage"    : 50,
-				"tradeAmount" : 100.00,
-				"amountType"  : "USD",
+				"orderAmount" : orderAmount,
+				"amountType"  : request.form.get("amountType"),
+				"leverage"    : leverage,
 				"notifications" : {
 					"emailNotifications"    : False,
 					"emailEntry"            : False,
@@ -60,8 +69,12 @@ def createbot():
 		mongo.db.Users.update_one({"_id": user['_id']}, {"$push": {"data.bots": botID}})
 
 		return redirect(f"/Bot/{botID}")
+		
 	else:
-		return redirect("/Login")
+		if 'username' in session:
+			return render_template('Dashboard/botcreate.html', user=user, apikey=False, apiSecret=False)
+		else:
+			return redirect("/Login")
 
 
 # Devuelve la pagina con informacion de un bot en especifico
@@ -70,7 +83,8 @@ def bot(id):
 	if request.method == 'POST':
 		user = mongo.db.Users.find_one({"username": session['username']})
 		bot  = mongo.db.Bots.find_one({"_id": ObjectId(id)})
-		
+		print("aqui empieza")
+  
   		# Activa el Bot
 		if request.form.get("action") == "active":
 			mongo.db.Bots.update_one(
@@ -85,10 +99,21 @@ def bot(id):
 
   		# Actualiza el Bot
 		elif request.form.get("action") == "update":
+			if (float(request.form.get("orderAmount")) > 100) and (request.form.get("amountType") == "%"): orderAmount = 100
+			elif (float(request.form.get("orderAmount")) < 0) and (request.form.get("amountType") == "%"): orderAmount = 0
+			else: orderAmount = float(request.form.get("orderAmount"))
+			
+			if int(request.form.get("leverage")) > 100: leverage = 100
+			elif int(request.form.get("leverage")) < 1: leverage = 1
+			else: leverage = int(request.form.get("leverage"))
+   
 			mongo.db.Bots.update_one(
 				{"_id": bot['_id']},
-				{"$set": {"exchangeData.apiKey2": request.form.get("apiKey2"),
-              			  "exchangeData.apiSecret2": request.form.get("apiSecret2")}})
+				{"$set": {"settings.exchange.apiKey2": request.form.get("apiKey2"),
+              			  "settings.exchange.secretKey2": request.form.get("secretKey2"),
+                   		  "settings.orderAmount": orderAmount,
+                          "settings.amountType": request.form.get("amountType"),
+                          "settings.leverage": leverage}})
 
   		# Elimina el Bot
 		elif request.form.get("action") == "delete":
@@ -99,6 +124,7 @@ def bot(id):
 		if (bot == None) or (bot['ownerID'] != user['_id']):
 			return render_template('404.html'), 404
 		else:
+			print("aqui termina")
 			return redirect(f'/Bot/{id}')
 
 	else:
